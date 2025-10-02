251ff1ed2a81afee1aecdb783543986d459fff3b
Zaki Asmat <zaki.asmat@sheridancollege.ca>
2025-10-02 08:22:12 -0400

    revision 1 local

---
+++ src/main/java/git/practicegit/PracticeGit.java
@@ -0,0 +1,13 @@
+package git.practicegit;
+
+/**
+ *
+ * @author Zaki Asmat
+ */
+public class PracticeGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is version 1.");
+    }
+}
