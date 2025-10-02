52a18d335fb09311380d7d379565f2faa8bc09ba
zakiasmat <zaki.asmat@sheridancollege.ca>
2025-10-02 09:10:33 -0400

    revision 4 remote

--- src/main/java/git/practicegit/PracticeGit.java
+++ src/main/java/git/practicegit/PracticeGit.java
@@ -8,6 +8,6 @@ public class PracticeGit
 {
     public static void main(String[] args)
     {
-        System.out.println("This is version 3.");
+        System.out.println("This is version 4.");
     }
 }
